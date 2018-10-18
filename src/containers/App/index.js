6efcd07f0df1939c959a53bca27cea417b0d36f3
Jan Vigar <jansvigar@gmail.com>
2018-10-18 00:24:03 -0700

    fix material ui next variant error

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import {Router} from '@reach/router';
+import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
 import HomePage from '../HomePage';
 
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true,
+  },
+  palette: {
+    primary: purple,
+    secondary: {
+      main: '#f44336',
+    },
+  },
+});
+
 const App = () => (
-  <Router>
-    <HomePage path="/" />
-  </Router>
+  <MuiThemeProvider theme={theme}>
+    <Router>
+      <HomePage path="/" />
+    </Router>
+  </MuiThemeProvider>
 );
 
 export default App;
